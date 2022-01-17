deef818be760552fd05a1984fbc689c82d8f53c1
mahesh-jangid <mesh.jangid@gmail.com>
2022-01-17 19:06:20 +0530

    Initial Commit

--- unit-4/sprint-4/evaluation/scripts/search.js
+++ unit-4/sprint-4/evaluation/scripts/search.js
@@ -1,6 +1,15 @@
 function storeSearchterm(term) {
+  try {
+    let res = await fetch(term);
 
+    let data = await res.json();
 
+    let response = data.articles;
+
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-export default storeSearchterm
+export default storeSearchterm;
